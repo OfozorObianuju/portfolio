f3353845671ee7cd2de91bf98aa3eeb50141a8c0
ofozorObianuju <ofozormerit1@gmail.com>
2019-11-13 14:57:14 +0100

    added github pages

---
+++ build/precache-manifest.ad428b4bb2a9b7861b3bbd491a94849d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6321ce77679b0f1e6323509712929a4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "10fddee7fd270d114256",
+    "url": "/portfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "a61b6524b3cef81d0bee",
+    "url": "/portfolio/static/css/main.941ae385.chunk.css"
+  },
+  {
+    "revision": "10fddee7fd270d114256",
+    "url": "/portfolio/static/js/2.4e111d2e.chunk.js"
+  },
+  {
+    "revision": "a61b6524b3cef81d0bee",
+    "url": "/portfolio/static/js/main.741eca27.chunk.js"
+  },
+  {
+    "revision": "9b0f5279e390e57c120c",
+    "url": "/portfolio/static/js/runtime-main.8f633534.js"
+  },
+  {
+    "revision": "86390c27a21b0b0bdc0a0804a39c3f97",
+    "url": "/portfolio/static/media/girlm.86390c27.png"
+  }
+]);
